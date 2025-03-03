2b56658d46e2d384672daf8610ec3bb05d9172b1
aniket44788 <darkpanda44788@gmail.com>
2025-03-03 19:03:37 +0530

    responsive

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,31 +8,31 @@ function Home() {
     <> <div className="main-box">
     <div className="div-icon"> <img src={mlogo} alt=""  className=" image-logo"/></div>
       <div className="home-nav">
-        <h1>
+        <h1 className = "nav-head">
           {" "}
           <Link to=""> HOME </Link>{" "}
         </h1>
-        <h1>
+        <h1 className = "nav-head">
           {" "}
           <Link to="/about"> ABOUT </Link>
         </h1>
-        <h1>
+        <h1 className = "nav-head">
           {" "}
           <Link to="/destination"> DESTINATION </Link>
         </h1>
-        <h1>
+        <h1 className = "nav-head">
           {" "}
           <Link to="/tour"> TOUR </Link>
         </h1>
-        <h1>
+        <h1 className = "nav-head">
           {" "}
           <Link to="/vlog"> VLOG </Link>
         </h1>
        
       </div>
-      <div className="button  "> <h1>
+      <div> <h1>
           {" "}
-          <Link to="/book"> SIGN UP   </Link>
+          <Link to="/book"> <button id="btn"> Sign Up</button> </Link>
         </h1></div>
         </div>
     </>
